435e7f591dda7bc35f17a54472a2b5e6dc92f3af
Abdelrahman Arafat <darkinb255@gmail.com>
2020-12-29 20:14:59 +0200

    updated common functions

--- notebooks/commonfunctions.py
+++ notebooks/commonfunctions.py
@@ -1,11 +1,12 @@
 
-
+import cv2
+import os
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.exposure import histogram
 from matplotlib.pyplot import bar
-from skimage.color import rgb2gray,rgb2hsv,rgba2rgb
+from skimage.color import rgb2gray, rgb2hsv, rgba2rgb
 from skimage import img_as_ubyte
 
 # Convolution:
@@ -21,31 +22,74 @@ from skimage.color import label2rgb
 
 
 # Edges
-from skimage.filters import sobel_h, sobel, sobel_v,roberts, prewitt
+from skimage.filters import sobel_h, sobel, sobel_v, roberts, prewitt
 
 # Show the figures / plots inside the notebook
-def show_images(images,titles=None):
-    #This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
+
+
+def show_images(images, titles=None):
+    # This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
     # images[0] will be drawn with the title titles[0] if exists
     # You aren't required to understand this function, use it as-is.
     n_ims = len(images)
-    if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+    if titles is None:
+        titles = ['(%d)' % i for i in range(1, n_ims + 1)]
     fig = plt.figure()
     n = 1
-    for image,title in zip(images,titles):
-        a = fig.add_subplot(1,n_ims,n)
-        if image.ndim == 2: 
+    for image, title in zip(images, titles):
+        a = fig.add_subplot(1, n_ims, n)
+        if image.ndim == 2:
             plt.gray()
         plt.imshow(image)
         a.set_title(title)
         n += 1
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
-    plt.show() 
+    plt.show()
 
 
 def showHist(img):
     # An "interface" to matplotlib.axes.Axes.hist() method
     plt.figure()
     imgHist = histogram(img, nbins=256)
-    
+
     bar(imgHist[1].astype(np.uint8), imgHist[0], width=0.8, align='center')
+
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
+
+
+def get_dimensions(w, h):
+    area = w * h
+    if (area < 400000):
+        return 15, 0
+    elif (999999 > area > 400000):
+        return 31, 0
+    else:
+        aspect = w / h
+        nw = np.sqrt(750000*aspect)
+        return 35, int(nw)
+
+
+def load_images_from_folder(folder):
+    images = []
+    files = []
+    print(folder)
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        if img is not None:
+            files.append(filename)
+            images.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+    return images, files
