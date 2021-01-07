aba780dd9cea99abb99195cd78ca41c424d07362
Abdelrahman Arafat <darkinb255@gmail.com>
2021-01-08 00:15:22 +0200

    isolation

--- notebooks/commonfunctions.py
+++ notebooks/commonfunctions.py
@@ -8,7 +8,12 @@ from skimage.exposure import histogram
 from matplotlib.pyplot import bar
 from skimage.color import rgb2gray, rgb2hsv, rgba2rgb
 from skimage import img_as_ubyte
-
+import matplotlib.pyplot as plt
+from skimage.morphology import binary_erosion, binary_dilation, binary_closing, skeletonize, thin, binary_opening
+from skimage.measure import find_contours
+import skimage.draw as draw
+import skimage.filters as filters
+import skimage.exposure as ex
 # Convolution:
 from scipy.signal import convolve2d
 from scipy import fftpack
@@ -23,6 +28,7 @@ from skimage.color import label2rgb
 
 # Edges
 from skimage.filters import sobel_h, sobel, sobel_v, roberts, prewitt
+
 
 # Show the figures / plots inside the notebook
 
@@ -93,3 +99,41 @@ def load_images_from_folder(folder):
             files.append(filename)
             images.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
     return images, files
+
+
+def integral_image(img):
+    rows, cols = img.shape
+    int_img = np.zeros(img.shape)
+    int_img[0][0] = img[0][0]
+    for r in range(1, rows):
+        int_img[r][0] = int_img[r-1][0] + img[r][0]
+    for c in range(1, cols):
+        int_img[0][c] = int_img[0][c-1] + img[0][c]
+    for r in range(1, rows):
+        for c in range(1, cols):
+            int_img[r][c] = img[r][c]+int_img[r][c-1] + \
+                int_img[r-1][c] - int_img[r-1][c-1]
+    return int_img
+
+
+def thresholding_segment(img, window, t=10):
+    rows, cols = img.shape
+    output = np.zeros(img.shape, dtype="uint8")
+    int_img = integral_image(img)
+    s = int(window/2)
+    p_img = np.pad(img, s, "constant")
+    p_int = np.pad(int_img, s, 'edge')
+    for r in range(s+1, rows+s):
+        for c in range(s+1, cols+s):
+            x1 = c-s
+            x2 = c+s
+            y1 = r-s
+            y2 = r+s
+            count = (x2-x1)*(y2-y1)
+            sum = p_int[y2, x2] - p_int[y2, x1-1] - \
+                p_int[y1-1, x2] + p_int[y1-1, x1-1]
+            if(img[r-s][c-s]*count) <= (sum*(100-t)/100):
+                output[r-s][c-s] = 0
+            else:
+                output[r-s][c-s] = 255
+    return output
